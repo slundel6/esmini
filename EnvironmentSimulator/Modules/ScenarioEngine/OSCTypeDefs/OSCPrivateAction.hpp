41279e2227da39be7d148fa975d4289ba954da18
slundel6 <slundel6@volvocars.com>
2025-04-11 14:01:23 +0200

    Dont snap to place too fast

--- EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.hpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.hpp
@@ -589,7 +589,8 @@ namespace scenarioengine
 
     private:
         double acceleration_;
-        bool   is_steering_ = false;
+        double   prev_lateral_distance_ = 0.0;
+        bool latched_to_target_ = false;
     };
 
     class LatLaneChangeAction : public OSCPrivateAction
