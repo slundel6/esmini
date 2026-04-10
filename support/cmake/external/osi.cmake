3636d97d01d2c613bc3ea8a1c727302d98cd8da8
slundel6 <slundel6@volvocars.com>
2026-04-10 10:55:03 +0200

    Clean up set_osi_libs macro

--- support/cmake/external/osi.cmake
+++ support/cmake/external/osi.cmake
@@ -4,154 +4,51 @@ include_guard()
 
 macro(set_osi_libs)
 
-    set(OSI_CONFIG_RESOLVED FALSE)
-
     # New Linux CI artifact layout:
-    #   externals/osi/linux/osi-deps
-    #   externals/osi/linux/build-osi-deps/vcpkg_installed/x64-linux
-    if(LINUX)
-        set(OSI_DEPS_PREFIX
-            ${EXTERNALS_OSI_OS_SPECIFIC_PATH}/osi-deps)
-        set(OSI_VCPKG_PREFIX
-            ${EXTERNALS_OSI_OS_SPECIFIC_PATH}/build-osi-deps/vcpkg_installed/x64-linux)
-        set(OSI_CONFIG_FILE
-            ${OSI_DEPS_PREFIX}/lib/cmake/open_simulation_interface-3/open_simulation_interface-config.cmake)
+    #   externals/osi/<platform>/osi-deps
+    #   externals/osi/linux/build-osi-deps/vcpkg_installed/<vcpkg_platform>
 
-        if(EXISTS
-           ${OSI_CONFIG_FILE}
-           AND EXISTS
-               ${OSI_VCPKG_PREFIX}/share/protobuf/protobuf-config.cmake)
-            file(GLOB OSI_RELEASE_TRANSITIVE_LIBS
-                "${OSI_VCPKG_PREFIX}/lib/libabsl_*.a"
-                "${OSI_VCPKG_PREFIX}/lib/libutf8*.a"
-                "${OSI_VCPKG_PREFIX}/lib/liblz4*.a"
-                "${OSI_VCPKG_PREFIX}/lib/libzstd*.a")
-            file(GLOB OSI_DEBUG_TRANSITIVE_LIBS
-                "${OSI_VCPKG_PREFIX}/debug/lib/libabsl_*.a"
-                "${OSI_VCPKG_PREFIX}/debug/lib/libutf8*.a"
-                "${OSI_VCPKG_PREFIX}/debug/lib/liblz4*.a"
-                "${OSI_VCPKG_PREFIX}/debug/lib/libzstd*.a")
+    set(OSI_DEPS_PREFIX
+        ${EXTERNALS_OSI_OS_SPECIFIC_PATH}/osi-deps)
 
-            if(CMAKE_CONFIGURATION_TYPES)
-                set(OSI_LIBRARIES
-                    debug     ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                    debug     -Wl,--start-group
-                    debug     ${OSI_VCPKG_PREFIX}/debug/lib/libprotobufd.a
-                    debug     ${OSI_DEBUG_TRANSITIVE_LIBS}
-                    debug     -Wl,--end-group
-                    optimized ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                    optimized -Wl,--start-group
-                    optimized ${OSI_VCPKG_PREFIX}/lib/libprotobuf.a
-                    optimized ${OSI_RELEASE_TRANSITIVE_LIBS}
-                    optimized -Wl,--end-group)
-            elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
-                set(OSI_LIBRARIES
-                    ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                    -Wl,--start-group
-                    ${OSI_VCPKG_PREFIX}/debug/lib/libprotobufd.a
-                    ${OSI_DEBUG_TRANSITIVE_LIBS}
-                    -Wl,--end-group)
-            else()
-                set(OSI_LIBRARIES
-                    ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                    -Wl,--start-group
-                    ${OSI_VCPKG_PREFIX}/lib/libprotobuf.a
-                    ${OSI_RELEASE_TRANSITIVE_LIBS}
-                    -Wl,--end-group)
-            endif()
+    # Search in vcpkg for protobuf dependencies (abseil, utf8, lz4 and zstd)
+    set(FULL_RELEASE_PATTERNS "")
+    set(FULL_DEBUG_PATTERNS "")
 
-            if(EXISTS
-               ${OSI_DEPS_PREFIX}/VERSION)
-                file(READ ${OSI_DEPS_PREFIX}/VERSION OSI_VERSION)
-                string(STRIP "${OSI_VERSION}" OSI_VERSION)
-            endif()
+    set(LIB_SEARCH_PATTERNS "libabsl_*.a" "libutf8*.a" "liblz4*.a" "libzstd*.a")
+    foreach(PATTERN ${LIB_SEARCH_PATTERNS})
+        list(APPEND FULL_RELEASE_PATTERNS "${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/lib/${PATTERN}")
+        list(APPEND FULL_DEBUG_PATTERNS "${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/debug/lib/${PATTERN}")
+    endforeach()
 
-            message(STATUS "OSI target: split CI artifact static libraries (version: ${OSI_VERSION})")
-            set(OSI_CONFIG_RESOLVED TRUE)
-        endif()
-    endif()
+    file(GLOB OSI_RELEASE_TRANSITIVE_LIBS ${FULL_RELEASE_PATTERNS})
+    file(GLOB OSI_DEBUG_TRANSITIVE_LIBS ${FULL_DEBUG_PATTERNS})
 
-    if(NOT OSI_CONFIG_RESOLVED)
-        if(APPLE)
-            if(DYN_PROTOBUF)
-                set(OSI_LIBRARIES
-                    ${EXTERNALS_OSI_LIBRARY_PATH}/release/libopen_simulation_interface.dylib
-                    ${EXTERNALS_OSI_LIBRARY_PATH}/release/libprotobuf.dylib)
-            else()
-                set(OSI_LIBRARIES
-                    ${EXTERNALS_OSI_LIBRARY_PATH}/release/libopen_simulation_interface_pic.a
-                    ${EXTERNALS_OSI_LIBRARY_PATH}/release/libprotobuf.a)
-            endif()
-
-        elseif(LINUX)
-            if(DYN_PROTOBUF)
-                set(OSI_LIBRARIES
-                    optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/libopen_simulation_interface.so
-                    optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/libprotobuf.so
-                    debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/libopen_simulation_interface.so
-                    debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/libprotobufd.so)
-            else()
-                if(EXISTS
-                   ${EXTERNALS_OSI_LIBRARY_PATH}/release/libopen_simulation_interface_pic.a)
-                    set(OSI_LIBRARIES
-                        optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/libopen_simulation_interface_pic.a
-                        optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/libprotobuf.a
-                        debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/libopen_simulation_interface_pic.a
-                        debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/libprotobufd.a)
-                else()
-                    # Fallback for split CI layout if package configs are absent.
-                    set(OSI_DEPS_PREFIX
-                        ${EXTERNALS_OSI_OS_SPECIFIC_PATH}/osi-deps)
-                    set(OSI_VCPKG_PREFIX
-                        ${EXTERNALS_OSI_OS_SPECIFIC_PATH}/build-osi-deps/vcpkg_installed/x64-linux)
-
-                    file(GLOB OSI_RELEASE_TRANSITIVE_LIBS
-                        "${OSI_VCPKG_PREFIX}/lib/libabsl_*.a"
-                        "${OSI_VCPKG_PREFIX}/lib/libutf8*.a"
-                        "${OSI_VCPKG_PREFIX}/lib/liblz4*.a"
-                        "${OSI_VCPKG_PREFIX}/lib/libzstd*.a")
-                    file(GLOB OSI_DEBUG_TRANSITIVE_LIBS
-                        "${OSI_VCPKG_PREFIX}/debug/lib/libabsl_*.a"
-                        "${OSI_VCPKG_PREFIX}/debug/lib/libutf8*.a"
-                        "${OSI_VCPKG_PREFIX}/debug/lib/liblz4*.a"
-                        "${OSI_VCPKG_PREFIX}/debug/lib/libzstd*.a")
-
-                    if(CMAKE_CONFIGURATION_TYPES)
-                        set(OSI_LIBRARIES
-                            debug     ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                            debug     -Wl,--start-group
-                            debug     ${OSI_VCPKG_PREFIX}/debug/lib/libprotobufd.a
-                            debug     ${OSI_DEBUG_TRANSITIVE_LIBS}
-                            debug     -Wl,--end-group
-                            optimized ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                            optimized -Wl,--start-group
-                            optimized ${OSI_VCPKG_PREFIX}/lib/libprotobuf.a
-                            optimized ${OSI_RELEASE_TRANSITIVE_LIBS}
-                            optimized -Wl,--end-group)
-                    elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
-                        set(OSI_LIBRARIES
-                            ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                            -Wl,--start-group
-                            ${OSI_VCPKG_PREFIX}/debug/lib/libprotobufd.a
-                            ${OSI_DEBUG_TRANSITIVE_LIBS}
-                            -Wl,--end-group)
-                    else()
-                        set(OSI_LIBRARIES
-                            ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
-                            -Wl,--start-group
-                            ${OSI_VCPKG_PREFIX}/lib/libprotobuf.a
-                            ${OSI_RELEASE_TRANSITIVE_LIBS}
-                            -Wl,--end-group)
-                    endif()
-                endif()
-            endif()
-
-        elseif(MSVC)
-            set(OSI_LIBRARIES
-                optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/open_simulation_interface_pic.lib
-                optimized ${EXTERNALS_OSI_LIBRARY_PATH}/release/libprotobuf.lib
-                debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/open_simulation_interface_pic.lib
-                debug     ${EXTERNALS_OSI_LIBRARY_PATH}/debug/libprotobufd.lib)
-        endif()
+    if(CMAKE_CONFIGURATION_TYPES)
+        set(OSI_LIBRARIES
+            debug     ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
+            debug     -Wl,--start-group
+            debug     ${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/debug/lib/libprotobufd.a
+            debug     ${OSI_DEBUG_TRANSITIVE_LIBS}
+            debug     -Wl,--end-group
+            optimized ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
+            optimized -Wl,--start-group
+            optimized ${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/lib/libprotobuf.a
+            optimized ${OSI_RELEASE_TRANSITIVE_LIBS}
+            optimized -Wl,--end-group)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set(OSI_LIBRARIES
+            ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
+            -Wl,--start-group
+            ${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/debug/lib/libprotobufd.a
+            ${OSI_DEBUG_TRANSITIVE_LIBS}
+            -Wl,--end-group)
+    else()
+        set(OSI_LIBRARIES
+            ${OSI_DEPS_PREFIX}/lib/libopen_simulation_interface_pic.a
+            -Wl,--start-group
+            ${EXTERNALS_OSI_OS_SPECIFIC_VCPKG_PATH}/lib/libprotobuf.a
+            ${OSI_RELEASE_TRANSITIVE_LIBS}
+            -Wl,--end-group)
     endif()
 endmacro()
