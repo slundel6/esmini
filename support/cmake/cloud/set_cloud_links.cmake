cab60d043b265474304ae4e0f4721bfb3ea4dc96
slundel6 <slundel6@volvocars.com>
2026-06-12 13:16:53 +0200

    Point to esmini-dependencies release

--- support/cmake/cloud/set_cloud_links.cmake
+++ support/cmake/cloud/set_cloud_links.cmake
@@ -12,27 +12,21 @@ macro(set_cloud_links)
             https://drive.usercontent.google.com/download?id=16fHAwCctVtaheLB5oqipuxSZvsIW_eUu&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/osg_mac_macos_13_png.7z;
             https://www.dropbox.com/scl/fi/pp8ml2pdhi26eyrzy91ki/osg_mac_macos_13_png.7z?rlkey=ejzcxlbrvl8gnqwih4ywsgfdo&st=i7799iip&dl=1)
-        if(DEFINED OSI_TAG_URL)
-            set(OSI_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${OSI_TAG_URL}/osi-macos.tar.xz)
-        endif()
+        set(OSI_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${OSI_TAG_URL}/osi-macos.tar.xz)
         set(SUMO_PACKAGE_URL
             https://drive.usercontent.google.com/download?id=1FAve0-MlJPv6lUZy0HvriZI7xstLAzvX&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/sumo_mac.7z;
             https://www.dropbox.com/s/0x8kwztk7nmacs1/sumo_mac.7z?dl=1)
-        if(DEFINED IMPLOT_TAG_URL)
-            set(IMPLOT_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${IMPLOT_TAG_URL}/implot-macos.tar.xz)
-        endif()
+        set(IMPLOT_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${IMPLOT_TAG_URL}/implot-macos.tar.xz)
     elseif(LINUX)
         set(OSG_PACKAGE_URL
             https://drive.usercontent.google.com/download?id=1NQlZBTFBWHpu-ImEjJN6CIxvwuinelzL&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/osg_linux_glibc_2_31_gcc_7_5_0_png.7z;
             https://www.dropbox.com/scl/fi/0rq5uefqmvfva0idsgsjn/osg_linux_glibc_2_31_gcc_7_5_0_png.7z?rlkey=x549jvuwljvgvivhbi0ysj5xj&st=qz6fll2i&dl=1)
-        if(DEFINED OSI_TAG_URL)
-            set(OSI_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${OSI_TAG_URL}/osi-linux.tar.xz)
-        endif()
+        set(OSI_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${OSI_TAG_URL}/osi-linux.tar.xz)
         set(SUMO_PACKAGE_URL
             https://drive.usercontent.google.com/download?id=1m4znxNIXapP0D-l21oIm2l7L5ti-JbZH&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/sumo_linux.7z;
@@ -41,19 +35,15 @@ macro(set_cloud_links)
             https://drive.usercontent.google.com/download?id=1Hyr9eJX2GmgpYwZhx14xOoXlZ2j-FY_p&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/googletest_linux.7z;
             https://dl.dropboxusercontent.com/s/si7jsjjsy5bpoym/googletest_linux.7z?dl=1)
-        if(DEFINED IMPLOT_TAG_URL)
-            set(IMPLOT_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${IMPLOT_TAG_URL}/implot-linux.tar.xz)
-        endif()
+        set(IMPLOT_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${IMPLOT_TAG_URL}/implot-linux.tar.xz)
     elseif(MSVC)
         set(OSG_PACKAGE_URL
             https://drive.usercontent.google.com/download?id=11QIsQkYbY0I8_YsivPH9r7_qqWBqbsfH&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/osg_win_vs17_v142_png.7z;
             https://www.dropbox.com/scl/fi/074keimjeec2e6jul3rn0/osg_win_vs17_v142_png.7z?rlkey=1631xcd9muiaayasf5p8jh597&st=ufyogs3h&dl=1)
-        if(DEFINED OSI_TAG_URL)
-            set(OSI_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${OSI_TAG_URL}/osi-windows.tar.xz)
-        endif()
+        set(OSI_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${OSI_TAG_URL}/osi-windows.tar.xz)
         set(SUMO_PACKAGE_URL
             https://drive.usercontent.google.com/download?id=18PhbSLyvs0IGWTAY3YBoYzpVnMFPbOuR&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/sumo_v10.7z;
@@ -62,10 +52,8 @@ macro(set_cloud_links)
             https://drive.usercontent.google.com/download?id=1So-3gtrmEdW9RhEvVQisj1QFksHM_otU&export=download&authuser=0&confirm=t;
             https://esmini.asuscomm.com/AICLOUD972108097/libs/googletest_v10.7z;
             https://dl.dropboxusercontent.com/s/aaiehwzc6woqbc6/googletest_v10.7z?dl=1)
-        if(DEFINED IMPLOT_TAG_URL)
-            set(IMPLOT_PACKAGE_URL
-                https://github.com/slundel6/esmini-sandbox/releases/download/${IMPLOT_TAG_URL}/implot-windows.tar.xz)
-        endif()
+        set(IMPLOT_PACKAGE_URL
+            https://github.com/esmini/esmini-dependencies/releases/download/${IMPLOT_TAG_URL}/implot-windows.tar.xz)
     elseif(MINGW)
         message("MinGW, enforcing slimmed esmini")
     else()
