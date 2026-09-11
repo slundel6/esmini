6251c7e694cc5913e20dd9bfd8b8f3662ff58599
slundel6 <slundel6@volvocars.com>
2026-09-11 14:38:02 +0200

    Fix bug where rear axle speed garbage on add entity

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/Entities.hpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/Entities.hpp
@@ -933,9 +933,9 @@ namespace scenarioengine
         std::shared_ptr<TrailerHitch>   trailer_hitch_;    // mounting point to any tow vehicle
         std::vector<WheelData>          wheel_data;
         double                          max_pitch_angle_ = 0.0;
-        SE_Vector                       rear_axle_pos_;    // rear axle position in world coordinates
-        SE_Vector                       rear_axle_vel_;    // rear axle velocity in world coordinates
-        double                          rear_axle_speed_;  // rear axle speed
+        SE_Vector                       rear_axle_pos_;          // rear axle position in world coordinates
+        SE_Vector                       rear_axle_vel_;          // rear axle velocity in world coordinates
+        double                          rear_axle_speed_ = 0.0;  // rear axle speed
     };
 
     class Pedestrian : public Object
